82eb1a36e1b16589dcc90e112ac012385c95df0d
Brad Morse <bkmorse@gmail.com>
2011-11-23 14:25:11 -0500

    added variables, v2.0

--- pi.image_directory.php
+++ pi.image_directory.php
@@ -12,7 +12,7 @@
 
 $plugin_info = array(
 	'pi_name'			=> 'Image Directory',
-	'pi_version'		=> '1.0.0',
+	'pi_version'		=> '2.0.0',
 	'pi_author'		=> 'Brad Morse',
 	'pi_author_url'	=> 'http://bkmorse.com',
 	'pi_description'	=> 'Displays all images from within a folder/directory on your server',
@@ -31,30 +31,61 @@ class Image_directory {
 	{
 		// make a local reference to the ExpressionEngine super object
 		$this->EE =& get_instance();
+		$this->EE->load->helper('file');
+		$this->EE->load->helper('path');
+		$this->EE->load->helper('url');
+		
+		$directory = (trim($this->EE->TMPL->fetch_param('directory')) !== false) ? $this->EE->TMPL->fetch_param('directory') : "";
+		$date_format = ($this->EE->TMPL->fetch_param('date_format') !== false) ? $this->EE->TMPL->fetch_param('date_format') : "F j, Y H:i a";
 		$img_ext = ($this->EE->TMPL->fetch_param('img_ext') !== false) ? $this->EE->TMPL->fetch_param('img_ext') : "jpg";
-		$directory = ($this->EE->TMPL->fetch_param('directory') !== false) ? $this->EE->TMPL->fetch_param('directory') : "";
-		$height = ($this->EE->TMPL->fetch_param('height') !== false) ? $this->EE->TMPL->fetch_param('height') : "";
-		$width = ($this->EE->TMPL->fetch_param('width') !== false) ? $this->EE->TMPL->fetch_param('width') : "";
-		$alt = ($this->EE->TMPL->fetch_param('alt') !== false) ? $this->EE->TMPL->fetch_param('alt') : "";
-		$class = ($this->EE->TMPL->fetch_param('alt') !== false) ? $this->EE->TMPL->fetch_param('alt') : "";
-		$wrap = ($this->EE->TMPL->fetch_param('wrap') !== false) ? $this->EE->TMPL->fetch_param('wrap') : "";
-    
-    $return_data = '';
 
-    if($img_ext && $directory) {
-  		//get all image files with a .jpg extension.
-      $images = glob($directory . "*.".$img_ext."");
-    
-      //print each file name
-      foreach($images as $image):
-       if($wrap != '') {
-       $return_data .= '<'.$wrap.'><img src="'.$image.'" height="'.$height.'" width="'.$width.'" alt="'.$alt.'" class="'.$class.'"></'.$wrap.'>';
-       } else {
-         $return_data .= '<img src="'.$image.'" height="'.$height.'" width="'.$width.'" alt="'.$alt.'" class="'.$class.'">';
-       }
+    $tagdata = '';
+    $var = '';
+    $return_data = '';
+		$img_types = array(
+			'gif'		=>	'image/gif',
+			'jpeg'	=>	'image/jpeg',
+			'jpg'		=>	'image/jpeg',
+			'png'		=>	'image/png'
+		);
+
+    $directory_exists = set_realpath($directory, TRUE);
+
+    if($directory_exists) {
+
+    	$variables = array();
+  	
+  		//get all files within directory
+      $file_names = get_dir_file_info($directory);
+      $total_rows = 0;
+      $row_count = 0;
+
+			
+      // get total # of files that contain $img_ext
+			foreach($file_names as $r):
+      	if(get_mime_by_extension($r['name']) == $img_types[$img_ext]):
+      		$total_rows++;
+      	endif;
       endforeach;
 
-  		$this->return_data = $return_data;
+      foreach($file_names as $r):
+      
+      	if(get_mime_by_extension($r['name']) == $img_types[$img_ext]):
+	        $variables[] = array(
+	  			  'filename' 			=>  $r['name'],
+	  			  'filesize' 	  	=>  $r['size'],
+	  			  'modified_on' 	=>  date($date_format, $r['date']),
+	  			  'image_count'   =>  $row_count,
+	  			  'total_images'  =>  $total_rows-1,
+	  			  'image_url'			=>	'/'.$r['relative_path'].$r['name']
+	  			);
+	  			
+	  			$row_count++;
+	  			
+	  			$this->return_data = $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $variables);
+				endif;
+
+      endforeach;
     }
 	}
 	/* END */
@@ -73,19 +104,29 @@ ob_start();
 ?>
 Use as follows:
 
-{exp:image_directory directory="/path/to/folder" img_ext="jpg" height="" width="" alt="" class="" wrap=""}
+{exp:image_directory directory="path/to/folder/" img_ext="jpg" date_format=""}
+  {if image_count == 0}<ul>{/if}
+	  <li><img src="{image_url}">{filename}, size: {filesize}, modified on {modified_on}</img></li>
+  {if image_count == total_images}</ul>{/if}
+{/exp:image_directory}
 
-directory (required) - absolute or relative path to folder of images you want to display
+parameters:
 
-img_ext (required) - gif|jpg|jpeg|png
+directory (required) - path starts at web root of your website, so it begins at the same level that your system folder does,
+also do not start the path with a '/' at the beginning, only at the end '/', as seen above in the directory parameter
 
-height, width, alt, class (optional)
+img_ext (required) - extensions supports gif, jpg, jpeg, png
 
-wrap (optional) - put within paragraph, div or span tags, see below
+date_format (optional) = php date format, this is to format the {modified_on} variable
 
-wrap="p" would put it within <p></p>
-wrap="div" would put it within <div></div>
-wrap="span" would put it within <span></span>
+variables:
+
+image_url - relative path to where file resides
+filename - filename.ext, example: image.jpg
+filesize - size in bytes
+modified_on - modified date of file, default date: November 11, 2011 11:00 am
+image_count - current count of row
+total_images - total number of images
 
 * if you get an error or nothing displays, be sure to check the path you passed to the directory parameter
 
